126afca624baf3194b3aa6eff4d4c058a6154bd8
fynjah <fynjah@gmail.com>
2020-02-13 14:56:52 +0200

    NDB init and models preparations.

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
-from google.cloud import datastore
+from google.cloud import ndb
 
 app = Flask(__name__)
 
-datastore_client = datastore.Client()
+client = ndb.Client()
+
+
+def serialize_todos(todos=[]):
+    return [dict(t.to_dict(), **dict(id=t.key.id())) for t in todos]
+
+
+class Todo(ndb.Model):
+    title = ndb.StringProperty()
+    timestamp = ndb.DateTimeProperty(auto_now_add=True)
+    checked = ndb.BooleanProperty(default=False)
 
 
 @app.route('/')
 def root():
-    dummy_times = []
+    todo = Todo(title="Lorem ipsum dolor sit amet, consectetur adipiscing.")
 
-    return render_template('index.html', times=dummy_times)
+    with client.context():
+        todo.put()
+        todos = Todo.query().order('-timestamp').fetch()
 
+    return render_template('index.html', times=todos)
+
+
+@app.route('/json')
+def json():
+    with client.context():
+        todos = Todo.query().order('-timestamp').fetch()
+    return jsonify(serialize_todos(todos))
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
