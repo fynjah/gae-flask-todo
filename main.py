da4de2db203ca0a9d8361428643840a7736e0827
fynjah <fynjah@gmail.com>
2020-02-18 10:33:52 +0200

    login_required decorator and small style fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, jsonify, session, request
+from functools import wraps
+from flask import Flask, render_template, jsonify, session, request, abort
 from google.cloud import ndb
 from google.oauth2 import id_token
 from google.auth.transport import requests
@@ -15,12 +16,20 @@ client = ndb.Client()
 
 
 def serialize_todo(todo):
-    # I know, som
     return dict(todo.to_dict(), **dict(id=todo.key.id()))
 
 
 def serialize_todos(todos=[]):
     return [serialize_todo(t) for t in todos]
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not 'userid' in session:
+            abort(400)
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/')
@@ -46,13 +55,13 @@ def auth():
         session['userid'] = userid
         return render_template('index.html')
     except ValueError:
-        return 'Error'
+        return jsonify({"error": "Token is not valid."}), 400
 
 
 @app.route('/api/v1/todos', methods=['POST', 'GET'])
+@login_required
 def api_todos():
-    if not 'userid' in session:
-        return 'Error not userid'
+    status_code = 200
     with client.context():
         if request.method == 'POST':
             title = request.values.get('title', None)
@@ -61,27 +70,27 @@ def api_todos():
             todo = Todo(title=request.values.get('title'),
                         userid=session['userid'])
             todo.put()
+            status_code = 201
         todos = Todo.query().filter(Todo.userid == session['userid'])
         todos = todos.fetch()
-    return jsonify(serialize_todos(todos))
+    return jsonify(serialize_todos(todos)), status_code
 
 
 @app.route('/api/v1/todos/<int:todo_id>', methods=['PUT', 'DELETE'])
+@login_required
 def api_todo_detail(todo_id):
-    if not 'userid' in session:
-        return 'Error not userid'
     userid = session['userid']
     with client.context():
         todo = Todo.get_by_id(todo_id)
         if not todo.userid == userid:
-            return '404'
+            return {"error": "Todo does not exist."}
         if request.method == 'PUT':
             todo.checked = bool(request.values.get('checked', False))
             todo.put()
             return jsonify(serialize_todo(todo))
         if request.method == 'DELETE':
             todo.key.delete()
-            return '204'
+            return None, 204
 
 
 if __name__ == '__main__':
